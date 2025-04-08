367063d1753312cb2b136d9a3c857dfca797e622
Mahyar JafariNodeh <136162130+mahyarjn80@users.noreply.github.com>
2025-04-08 15:58:07 -0400

    bug fix

--- src/utilities.py
+++ src/utilities.py
@@ -20,7 +20,7 @@ def get_gd_directory(dataset: str, lr: float, arch_id: str, seed: int, opt: str,
     """Return the directory in which the results should be saved."""
     results_dir = os.environ["RESULTS"]
     directory = f"{results_dir}/{dataset}/{arch_id}/seed_{seed}/{loss}/{opt}/"
-    if opt == "gd":
+    if opt == "gd" or opt == "muon":
         return f"{directory}/lr_{lr}"
     elif opt == "polyak" or opt == "nesterov":
         return f"{directory}/lr_{lr}_beta_{beta}"
